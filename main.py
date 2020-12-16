68f98dd4b6196cefa188de15ef52f440b526b570
ebosetalee <princess01ella.miley@gmail.com>
2020-12-16 23:28:56 +0100

    corrections made

--- main.py
+++ main.py
@@ -46,29 +46,29 @@ def get_recipie(name):
     return jsonify({"status": "success", "data": one_recipie})
 
 
-@app.route("/recipies/<idnumber>", methods=["PUT", "DELETE"])
-def update_recipie(idnumber):
+@app.route("/recipies/<id_number>", methods=["PUT", "DELETE"])
+def update_recipie(id_number):
     if request.method == "PUT":
         data = request.get_json()
-        update_data = Recipie.update(food_name = data["name"], description = data["description"], top_image = data["top image"], bottom_image = data["bottom image"].where(Recipie.id == idnumber))
+        update_data = Recipie.update(food_name = data["name"], description = data["description"], top_image = data["top image"], bottom_image = data["bottom image"].where(Recipie.id == id_number))
         update_data.execute()
-        query = Recipie.select().where(Recipie.id == idnumber).tuples()
+        query = Recipie.select().where(Recipie.id == id_number).tuples()
         updated_recipie = []
         for item in query:
             updated_recipie.append(item)
         return jsonify({"status": "success", "data": updated_recipie})
     elif request.method == "DELETE":
-        recipie = Recipie.get(Recipie.id == idnumber)
+        recipie = Recipie.get(Recipie.id == id_number)
         recipie.delete_instance()
-        return Recipie.get_or_none(Recipie.id == idnumber)
+        return Recipie.get_or_none(Recipie.id == id_number)
 
 
 @app.route("/recipies/<name>/<act>", methods=["GET", "POST"])
 def get_act(name, act):
     key = Recipie.get(Recipie.food_name == name).id
-    if act == "ingredients":
-        if request.method == "POST":
-            data = request.get_json()
+    if request.method == "POST":
+        data = request.get_json()
+        if act == "ingredients":
             try:
                 if not data.get("category"):
                     data["category"] = "null"
@@ -81,14 +81,8 @@ def get_act(name, act):
                 Ingredient.create(food_id = key, category = data["category"], name = data["name"], quantity = data["quantity"], Type=data["type"], requirement = data["requirement"], size = data["size"])
                 return jsonify({"status": "success", "data": data}), 201
             except:
-                return jsonify({"status": "error", "error": {"Keys(in this order)": ["category(optional)", "name(important)", "quantity(important)", "type(optional)", "requirement(optional)", "size(optional)"]}}), 404
-        query = Ingredient.select().where(Ingredient.food_id == key).dicts()
-        step_ingre = []
-        for item in query:
-            step_ingre.append(item)
-    elif act == "steps":
-        if request.method == "POST":
-            data = request.get_json()
+                return jsonify({"status": "error", "error": {"Keys(in this order)": ["category(optional)", "name(important)", "quantity(important)", "type(optional)", "requirement(optional)", "size(optional)"]}}), 404        
+        elif act == "steps":
             try:
                 if not data.get("category"):
                     data["category"] = "null"
@@ -100,17 +94,26 @@ def get_act(name, act):
                 return jsonify({"status": "success", "data": data}), 201
             except:
                 return jsonify({"status": "error", "error": {"Keys(in this order)": ["category(optional)", "name(optional)", "action(important)", "image(optional)"]}}), 404
-        query = Step.select().where(Step.food_id == key).dicts()
-        step_ingre = []
-        for item in query:
-            step_ingre.append(item)
+        else:
+            return jsonify({"status": "error", "Key Error": "Act available in database: steps and ingredients"})  
     else:
-        return jsonify({"status": "error", "Key Error": "Act available in database: steps and ingredients"})
-    return jsonify({"status": "success", f"{act}": step_ingre})
+        if act == "ingredients":  
+            query = Ingredient.select().where(Ingredient.food_id == key).dicts()
+            step_ingre = []
+            for item in query:
+                step_ingre.append(item)
+        elif act == "steps":
+            query = Step.select().where(Step.food_id == key).dicts()
+            step_ingre = []
+            for item in query:
+                step_ingre.append(item)
+        else:
+            return jsonify({"status": "error", "Key Error": "Act available in database: steps and ingredients"})  
+        return jsonify({"status": "success", f"{act}": step_ingre})
 
 
-@app.route("/recipies/<act>/<idnumber>", methods=["PUT", "DELETE"])
-def update_act(act, idnumber):
+@app.route("/recipies/<act>/<id_number>", methods=["PUT", "DELETE"])
+def update_act(act, id_number):
     if request.method == "PUT":
         data = request.get_json()
         if act == "ingredients":  
@@ -122,7 +125,7 @@ def update_act(act, idnumber):
                 data["requirement"] = "null"
             if not data.get("size"):
                 data["size"] = "null"
-            Ingredient.update(category = data["category"], name = data["name"], quantity = data["quantity"], Type=data["type"], requirement = data["requirement"], size = data["size"].where(Ingredient.id == idnumber))
+            Ingredient.update(category = data["category"], name = data["name"], quantity = data["quantity"], Type=data["type"], requirement = data["requirement"], size = data["size"].where(Ingredient.id == id_number))
             return jsonify({"status": "success", "data": data}), 201
         elif act == "steps":
             if not data.get("category"):
@@ -131,19 +134,19 @@ def update_act(act, idnumber):
                 data["name"] = "null"
             if not data.get("image"):
                 data["image"] = "null"
-            Step.update(category = data["category"], name = data["name"], action = data["action"], image = data["image"].where(Step.id == idnumber))
+            Step.update(category = data["category"], name = data["name"], action = data["action"], image = data["image"].where(Step.id == id_number))
             return jsonify({"status": "success", "data": data}), 201
         else:
             return jsonify({"status": "error", "Key Error": "Act available in database: steps and ingredients"})
     elif request.method == "DELETE":
         if act == "ingredients":
-            ingredient = Ingredient.get(Ingredient.id == idnumber)
+            ingredient = Ingredient.get(Ingredient.id == id_number)
             ingredient.delete_instance()
-            return jsonify({"status": "success", "data": (Ingredient.get_or_none(Ingredient.id == idnumber))}), 201 
+            return jsonify({"status": "success", "data": (Ingredient.get_or_none(Ingredient.id == id_number))}), 201 
         elif act == "steps":
-            step = Step.get(Step.id == idnumber)
+            step = Step.get(Step.id == id_number)
             step.delete_instance()
-            return jsonify({"status": "success", "data": Step.get_or_none(Step.id == idnumber)}), 201
+            return jsonify({"status": "success", "data": Step.get_or_none(Step.id == id_number)}), 201
         else:
             return jsonify({"status": "error", "Key Error": "Act available in database: steps and ingredients"})
 
